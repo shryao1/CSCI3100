f93efe1546c2aefa0c03de51e3fc677b82bbed62
Lin Yuan <52481311+yuanlin0710@users.noreply.github.com>
2024-03-30 21:51:32 +0800

    [frontend] package config and endpoint

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import App from './Routes/App'
+
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
+
+reportWebVitals()
