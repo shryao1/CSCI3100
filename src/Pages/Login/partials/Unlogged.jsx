10e187044027875854b233e4f3842f7b740fedfa
Winniehyw <1155157280@link.cuhk.edu.hk>
2024-04-04 21:16:21 +0800

    Updated the Login Page and connected it to the backend

--- src/Pages/Login/partials/Unlogged.jsx
+++ src/Pages/Login/partials/Unlogged.jsx
@@ -1,26 +1,36 @@
-import { verificationAccount } from '../../../Services/api'
+import { useState } from 'react'
 
+const Unlogged = ({ setLogin, navigate }) => {
+	const [userID, setUserID] = useState('')
+	const [password, setPassword] = useState('')
 
-const Unlogged = ({
-	dataInput,
-	setDataInput,
-	setLogin,
-	setResponse
-}) => {
-
-	const handleChangeInput = (e) => {
-		setDataInput(e.target.value)
-	}
 	const verificationExistsAccount = async () => {
-		if (dataInput !== '') {
-			let response = await verificationAccount({
-				username: `@${dataInput}`,
-				email: dataInput,
-				phone: dataInput
-			})
-			if (response !== false) {
-				setLogin(true)
-				setResponse(response)
+		if (password !== '' && userID !== '') {
+			try {
+				const response = await fetch('http://localhost:3001/login', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					
+					body: JSON.stringify({
+						userID,
+						password,
+					}),
+				})
+
+				const data = await response.json()
+
+				if (response.ok) {
+					localStorage.setItem('userTwitterClone', data.userID)
+					navigate('/home')
+				} else {
+					// Handle login failure (you might want to set a state to show the error message)
+					console.log(data.message) // Assuming the backend sends back a message field on error
+				}
+			} catch (error) {
+				console.error('Failed to log in:', error)
+				// Handle login failure (you might want to set a state to show the error message)
 			}
 		}
 	}
@@ -33,40 +43,35 @@ const Unlogged = ({
 			<div className="login_divider">
 				<div className="divider"></div>
 			</div>
-			<div className="login_enter_mail">
-				<label className={`login_info ${dataInput !== '' ? 'show' : 'hide'}`}>
-					<span className="login_span_info">Email or Username</span>
+
+			{/* Your form elements and input handlers */}
+			<div className="login_enter_userid">
+				<label className={`login_info ${userID !== '' ? 'show' : 'hide'}`}>
+					<span className="login_span_info">User ID</span>
 				</label>
 				<input type="text"
 					className="enter_login_info"
-					value={dataInput}
-					onChange={(e) => handleChangeInput(e)}
-					placeholder="       Email or Username"
+					value={userID}
+					onChange={(e) => setUserID(e.target.value)}
+					placeholder="    UserID"
 					required
 				/>
-
 			</div>
 			<div className="login_enter_password">
-				<label className={`login_info ${dataInput !== '' ? 'show' : 'hide'}`}>
+				<label className={`login_info ${password !== '' ? 'show' : 'hide'}`}>
 					<span className="login_span_info">Password</span>
 				</label>
-				<input type="text"
-					className="enter_login_info"
-					value={dataInput}
-					onChange={(e) => handleChangeInput(e)}
-					placeholder="       Password"
+				<input type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="    Password"
 					required
 				/>
-
 			</div>
 			<div className="login_button_login">
 				<label type="button" className="login_button" onClick={() => verificationExistsAccount()}>
 					<span className="login_span">Login</span>
 				</label>
-			</div>
-			<div className="login_signup">
-				<span className="login_message">You do not have an account?</span>
-				<span className="login_link">Sign up</span>
 			</div>
 		</div>
 	)
