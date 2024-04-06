1213b07877f4b2effe48927edbce096ea2e5751a
KayceYu <95004539+KayceYu@users.noreply.github.com>
2024-04-07 02:02:34 +0800

    Update new tweet photo

--- src/shared/Components/NewTweet/NewTweet.js
+++ src/shared/Components/NewTweet/NewTweet.js
@@ -1,26 +1,32 @@
-import { useContext, useState } from 'react'
-
+import React, { useContext, useState, useEffect, useRef } from 'react'
 import { AppContext } from '../../../Context/AppContext'
-
 import PhotoUser from '../PhotoUser/PhotoUser'
 import TextBlue from '../TextBlue/TextBlue'
 import BtnTwitter from '../BtnTwitter/BtnTwitter'
-
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined'
+import IconButton from '@mui/material/IconButton'
 import GifBoxOutlinedIcon from '@mui/icons-material/GifBoxOutlined'
 import PollOutlinedIcon from '@mui/icons-material/PollOutlined'
 import SentimentSatisfiedAltOutlinedIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined'
 import DateRangeOutlinedIcon from '@mui/icons-material/DateRangeOutlined'
 import AddLocationAltOutlinedIcon from '@mui/icons-material/AddLocationAltOutlined'
-
+import './App.css'
 import './NewTweet.scss'
-
+import ImagePosted from '../ImagePosted/ImagePosted'
 const NewTweet = ({
 	placeholder = 'Place Holder',
 	isComment,
 	toUser,
 	idPost
 }) => {
+	const [files, setFiles] = useState([])
+  	const [selectedFile, setSelectedFile] = useState(null)
+  	const [previewUrl, setPreviewUrl] = useState('')
+	const fileInputRef = useRef(null)
+  	useEffect(() => {
+    	fetchFiles()
+  	}, [])
+
 	const appContext = useContext(AppContext)
 
 
@@ -29,7 +35,63 @@ const NewTweet = ({
 	const handleChangeInput = (e) => {
 		setTextPost(e.target.value)
 	}
+	async function fetchFiles() {
+		try {
+		  const response = await fetch('http://localhost:3001/files')
+		  if (!response.ok) {
+				throw new Error('Network response was not ok')
+		  }
+		  const files = await response.json()
+		  setFiles(files)
+		} catch (error) {
+		  console.error('Failed to fetch files:', error)
+		}
+	}
+	/*
+	function handleFileChange(event) {
+		setSelectedFile(event.target.files[0])
+	}*/
+	function handleFileChange(event) {
+		const file = event.target.files[0]
+		if (file) {
+		  const fileUrl = URL.createObjectURL(file)
+		  setPreviewUrl(fileUrl)
+		  ImagePosted({ fileUrl })
+		}
+	}
 
+	async function handleFileUpload(event) {
+		event.preventDefault()
+		if (!selectedFile) return
+	
+		const formData = new FormData()
+		formData.append('file', selectedFile)
+	
+		try {
+		  const response = await fetch('http://localhost:3001/upload', {
+				method: 'POST',
+				body: formData,
+		  })
+	
+		  if (!response.ok) {
+				throw new Error('Upload failed')
+		  }
+	
+		  fetchFiles() // Refresh the list after uploading
+		  setSelectedFile(null) // Reset selected file
+		} catch (error) {
+		  console.error('Upload error:', error)
+		}
+	}
+	function handleButtonClick() {
+		fileInputRef.current.click() // 当按钮被点击时，触发文件输入框的点击事件
+	  }
+	function handleFileClick(fileId) {
+		// Construct the URL for the image
+		const fileUrl = `http://localhost:3001/file/${fileId}`
+		setPreviewUrl(fileUrl)
+		ImagePosted({fileUrl})
+	}
 	return (
 		<div className="newTweet__container">
 			<section className="main__navHome">
@@ -62,8 +124,17 @@ const NewTweet = ({
 						<div className="formNavHome__options">
 							<div className="formNavHome__options-icons">
 								<div>
-									<ImageOutlinedIcon />
-								</div>
+      							<input
+       	 						type="file"
+       	 						ref={fileInputRef}
+        						style={{ display: 'none' }}
+        						onChange={handleFileChange}
+      								/>
+      								<IconButton onClick={handleButtonClick}>
+        							<ImageOutlinedIcon />
+      							</IconButton>
+      							
+    							</div>
 								<div>
 									<GifBoxOutlinedIcon />
 								</div>
@@ -98,8 +169,26 @@ const NewTweet = ({
 					</div>
 				</div>
 			</section>
+			<h1>Selected photo</h1>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				{previewUrl && <img src={previewUrl} alt="Preview" width="50"/>}
+			</div>
 		</div>
 	)
 }
-
+/*return (
+    	<div>
+      	<input
+       	 type="file"
+       	 ref={fileInputRef}
+        	style={{ display: 'none' }}
+        	onChange={handleFileChange}
+      		/>
+      		<IconButton onClick={handleButtonClick}>
+        	<ImageOutlinedIcon />
+      		</IconButton>
+      		{previewUrl && <img src={previewUrl} alt="Preview" />}
+    	</div>
+  		)
+}*/
 export default NewTweet
