87bd89d7c4f7872dd8a31780d10c73d173f6c91c
Lin Yuan <52481311+lin0yuan@users.noreply.github.com>
2024-04-07 22:49:19 +0800

    [frontend] debug url
    debug url wrong path

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const PRODUCTION_URL = 'http://localhost:3001'
-const LOCAL_URL = 'http://localhost:3001/api'
+const LOCAL_URL = 'http://localhost:3001'
 
 export const getAllPost = async () => {
 	try {
