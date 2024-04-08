240a73888d711b64abda9f7f250846df103596d4
Ficus YAO <91064164+shryao1@users.noreply.github.com>
2024-04-08 22:05:20 +0800

    Update useILikeThis.js

--- src/Hooks/useILikeThis.js
+++ src/Hooks/useILikeThis.js
@@ -1,11 +1,19 @@
-import { useContext } from 'react'
+import { GetUserA } from '../Services/api'
+import { useContext,useEffect,useState } from 'react'
 import { AppContext } from '../Context/AppContext'
 
-const useILikeThis = (likes) => {
-	const appContext = useContext(AppContext)
-	if (likes) {
-		return likes.find(like => like.username === appContext?.user?.username)
-	}
+const useILikeThis = (id, userID) => {
+	const [ userLikes, setUserLike ] = useState(false)
+	const user = GetUserA(userID)
+	console.log(user)
+	useEffect(() => {
+		user.then(
+			data=>
+				setUserLike(data.likePost.includes(id.toString()))
+		)
+	})
+	console.log(userLikes)
+	return userLikes
 }
 
 export default useILikeThis
