07652e13e4cb17074cd7b184e5d1c6d6562c5290
KayceYu <sad1042872008@163.com>
2024-04-08 22:05:42 +0800

    Update Explore.js

--- src/Pages/Explore/Explore.js
+++ src/Pages/Explore/Explore.js
@@ -14,13 +14,22 @@ const Explore = () => {
                 	return response.json()
             	})
             	.then((data) => {
-                	setPostData(data)
+                	const shuffledData = shuffleArray(data)
+                	setPostData(shuffledData)
                 	//console.log('here is test',data)
             	})
             	.catch((error) => {
                		console.error('Error fetching user data:', error)
             	})
       	}, [])
+	const shuffleArray = (array) => {
+        	const shuffledArray = [...array]
+        	for (let i = shuffledArray.length - 1; i > 0; i--) {
+            	const j = Math.floor(Math.random() * (i + 1))
+            	;[shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+        	}
+        	return shuffledArray
+    	}
 	return (
 		<div className="explore__container">
 			<NavExplore />
