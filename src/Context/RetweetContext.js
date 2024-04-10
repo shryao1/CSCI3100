2476a726d3f2cfbf86d4b8408990699077f770c3
Ficus YAO <91064164+shryao1@users.noreply.github.com>
2024-04-10 23:42:34 +0800

    Update RetweetContext.js

--- src/Context/RetweetContext.js
+++ src/Context/RetweetContext.js
@@ -1,1 +1,36 @@
 
+import React, { createContext, useState, useEffect } from 'react'
+
+export const RetweetContext = createContext({})
+
+const RetweetProvider = ({ children }) => {
+
+	const [text, setText] = useState(null)
+	const [attachment, setAttachment] = useState(null)
+	const [thereistext, setThereIsText] = useState(false)
+	const [thereisattachment, setThereIsAttachment] = useState(false)
+	const initialstate = {
+		text, setText,
+		attachment, setAttachment,
+		thereistext, setThereIsText,
+		thereisattachment, setThereIsAttachment
+	}
+	useEffect(() => {
+		setText('')
+		setAttachment(null)
+		setThereIsText(false)
+		setThereIsAttachment(false)
+	},[])
+
+	const clearText = () => {
+		setText({ text: null })
+		setThereIsText({thereistext: false})
+	}
+	return (
+		<RetweetContext.Provider value={initialstate}>
+			{children}
+		</RetweetContext.Provider>
+	)
+}
+
+export default RetweetProvider
