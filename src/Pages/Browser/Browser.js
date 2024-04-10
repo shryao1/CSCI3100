9f99686d57ceb23eda84d5d1d7e89ab262a1da01
KayceYu <sad1042872008@163.com>
2024-04-10 18:19:05 +0800

    display "selected" after select the photo

--- src/Pages/Browser/Browser.js
+++ src/Pages/Browser/Browser.js
@@ -3,10 +3,13 @@ import { useParams } from 'react-router-dom'
 import NavBrowser from '../../Components/NavPages/NavBrowser/NavBrowser'
 import TweetPost from '../../Components/Tweet/TweetPost/TweetPost'
 import './Browser.scss'
+import sad from './sad.png'
+import { set } from 'mongoose'
 
 const Browser = () => {
 	const { userID } = useParams()
 	const [postData, setPostData] = useState(null)
+	const [showNotFound, setShowNotFound] = useState(false)
 	useEffect(() => {
         	fetch(`http://localhost:3001/browser/${userID}`)
             	.then((response) => {
@@ -17,7 +20,8 @@ const Browser = () => {
             	})
             	.then((data) => {
                 	setPostData(data)
-                	console.log('here is test',data)
+				setShowNotFound(true)
+                	//console.log('here is test',data)
             	})
             	.catch((error) => {
                		console.error('Error fetching user data:', error)
@@ -30,6 +34,13 @@ const Browser = () => {
 				{postData?.map((post) => {
 					return <TweetPost key={post.postID} post={post} />
 				})}
+				{showNotFound === false && 
+				<>
+					<br></br>
+					<h2>Sorry, your followings have not send posts now.</h2>
+					<img src={sad} alt="something wrong" className="search-logo" style={{ width: 320, height: 320 }} />
+				</>
+				}
 			</div>
 		</div>
 	)
